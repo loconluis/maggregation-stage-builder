d84b96cb7cf7e459a3838db6dfa3de70fd14f9dd
Luis Locon <luis.locon@gmail.com>
2019-11-25 19:35:08 -0600

    [first commit]

---
+++ index.js
@@ -0,0 +1,180 @@
+class queryBuilder {
+  constructor() {
+    this.PIPELINE = [];
+  }
+  /**
+   *
+   * @param {*} item
+   */
+  pipePush(item) {
+    this.PIPELINE.push(item);
+    return this;
+  }
+
+  /**
+   *
+   */
+  clone() {
+    let copy = new queryBuilder();
+    copy.PIPELINE = [...this.PIPELINE];
+    return Object.assign(Object.getPrototypeOf(copy));
+  }
+
+  lookup(from, localField, foreignField, as) {
+    return this.pipePush({
+      $lookup: {
+        from,
+        localField,
+        foreignField,
+        as
+      }
+    });
+  }
+  /**
+   *
+   * @param {*} pathKey
+   * @param {*} preserveNullAndEmptyArrays
+   */
+  unwind(pathKey, preserveNullAndEmptyArrays = false) {
+    return this.pipePush({
+      $unwind: {
+        path: pathKey,
+        preserveNullAndEmptyArrays
+      }
+    });
+  }
+
+  /**
+   *
+   * @param {*} key
+   * @param {*} value
+   * @param {*} operator
+   */
+  matchSimple(key, value, operator = "$in") {
+    let aux = {};
+    if (!key || !value) return this;
+    aux[_key] = Array.isArray(value) ? { $in: value } : value;
+    return this.pipePush({ $match: aux });
+  }
+
+  /**
+   *
+   * @param {*} query
+   */
+  groupBy(query) {
+    return this.pipePush({ $group: query });
+  }
+
+  /**
+   *
+   * @param {*} number
+   */
+  limit(number = 1) {
+    return isNaN(number) ? this : this.pipePush({ $limit: parseInt(number) });
+  }
+  /**
+   *
+   * @param {*} number
+   */
+  skip(number = 0) {
+    return isNaN(number) ? this : this.pipePush({ $skip: parseInt(number) });
+  }
+  /**
+   *
+   * @param {*} specifications
+   */
+  project(specifications = {}) {
+    return this.pipePush({ $project: specifications });
+  }
+  /**
+   *
+   * @param {*} specifications
+   */
+  sort(specifications = {}) {
+    return this.pipePush({ $sort: specifications });
+  }
+  /**
+   *
+   * @param {*} key
+   */
+  count(key = "") {
+    return this.pipePush({ $count: key });
+  }
+  /**
+   *
+   * @param {*} _raw
+   */
+  pushRaw(_raw) {
+    return this.pipePush(_raw);
+  }
+  /**
+   *
+   */
+  getPipe() {
+    return this.PIPELINE;
+  }
+  /**
+   *
+   */
+  addFields(expression = {}) {
+    return this.pipePush({ $addFields: expression });
+  }
+  /**
+   * 
+   * @param {*} groupBy 
+   * @param {*} boundaries 
+   * @param {*} defaultKey 
+   * @param {*} outputDocument 
+   */
+  bucket(
+    groupBy,
+    boundaries = [],
+    defaultKey = undefined,
+    outputDocument = {}
+  ) {
+    return this.pipePush({
+      $bucket: {
+        groupBy,
+        boundaries,
+        default: defaultKey,
+        output: outputDocument
+      }
+    })
+  }
+  /**
+   * 
+   * @param {*} groupBy 
+   * @param {*} boundaries 
+   * @param {*} defaultKey 
+   * @param {*} outputDocument 
+   * @param {*} granularity 
+   */
+  bucketAuto(
+    groupBy,
+    boundaries = [],
+    defaultKey = undefined,
+    outputDocument = {},
+    granularity= ''
+  ) {
+    return this.pipePush({
+      $bucket: {
+        groupBy,
+        boundaries,
+        default: defaultKey,
+        output: outputDocument,
+        granularity
+      }
+    })
+  }
+  /**
+   * 
+   * @param {*} specifications 
+   */
+  facet(specifications = {}) {
+    return this.pipePush({
+      $facet: specifications
+    })
+  }
+
+  
+}
